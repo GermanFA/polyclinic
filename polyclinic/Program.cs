9df9ffd845df15b1a3da0cb205b314cbb152d561
GermanFA <79627686+GermanFA@users.noreply.github.com>
2021-02-26 05:59:31 +0300

    Добавьте файлы проекта.

---
+++ polyclinic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace polyclinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int old_ladies;
+            int time_hour;
+            int time_minute;
+            int time_work = 10;
+
+            Console.Write("Введите длину очереди:");
+            old_ladies = Convert.ToInt32(Console.ReadLine());
+            time_hour = (old_ladies * time_work)/60;
+            time_minute = (old_ladies * time_work) - (time_hour*60);
+            Console.Write("Время ожидания в очереди " + time_hour + " часов и " + time_minute + " минут" );
+
+
+        }
+    }
+}
